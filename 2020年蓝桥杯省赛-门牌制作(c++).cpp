1261dd1e820f4603dfe106a8a787b26c0a10c0a1
Zeta0080-cell <2489889104@qq.com>
2025-03-01 13:34:37 +0800

    Add files via upload

---
+++ 2020年蓝桥杯省赛-门牌制作(c++).cpp
@@ -0,0 +1,23 @@
+//2020年蓝桥杯省赛-门牌制作(c++)
+//to_string函数 
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+	int count;
+	for(int i=1;i<=2020;i++)
+	{
+		string s = to_string(i);
+		for(int j=0;i<s.size();j++)
+		{
+			if(s[j]=='2')
+			{
+				count++;
+			}
+		}
+	}
+	cout<<count<<endl;
+	return 0;
+}
