4cf28b9d3f56c96c020a78679a9357869e513939
Zeta0080-cell <2489889104@qq.com>
2025-04-26 20:58:23 +0800

    Add files via upload

---
+++ 单链表—尾插法11.cpp
@@ -0,0 +1,56 @@
+//单链表—尾插法11
+#include<bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+typedef struct node{
+	ElemType data;
+	struct node *next;
+}Node; 
+
+Node* initlist()
+{
+	Node *head=(Node*)malloc(sizeof(Node));
+	head->data=0;
+	head->next=NULL;
+	return head;
+}
+
+void nodelist(Node *L)//链表遍历 
+{
+	Node *p=L->next;
+	while(p!=NULL)
+	{
+		printf("%d ",p->data);
+		p=p->next;
+	}
+}
+
+Node* get_tail(Node *L)
+{
+	Node *p=L;
+	while(p->next!=NULL)
+	{
+		p=p->next;
+	}
+	return p;
+}
+
+Node* insertTail(Node *tail,ElemType e)
+{
+	Node *p=(Node*)malloc(sizeof(Node));
+	p->data=e;
+	tail->next=p;
+	p->next=NULL;
+	return p;
+}
+
+int main()
+{
+	Node *list=initlist();
+	Node *tail=get_tail(list);
+	tail=insertTail(tail,1);
+	tail=insertTail(tail,2);
+	tail=insertTail(tail,3);
+	nodelist(list);
+	return 0;
+}
