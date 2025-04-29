59013c11aab2569cfc5fb02bf03131ba297ab6fc
Zeta0080-cell <2489889104@qq.com>
2025-04-29 23:05:15 +0800

    Add files via upload

---
+++ 单链表应用-查找倒数第k个结点.cpp
@@ -0,0 +1,68 @@
+//单链表应用-查找倒数第k个结点
+#include<bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+typedef struct node{
+	ElemType data;
+	struct node *next;
+}Node; 
+
+Node* initlist()
+{
+	Node *head=(Node*)malloc(sizeof(Node));
+	head->data=0;
+	head->next=NULL;
+	return head;
+}
+
+
+int insertNode(Node *L,int pos,ElemType e)
+{
+	Node *p=L;
+	int i=0;
+	while(i<pos-1)
+	{
+		p=p->next;
+		i++;
+		if(p==NULL)
+		{
+			return 0;
+		}
+	}
+	Node *q=(Node*)malloc(sizeof(Node));
+	q->data=e;//插入数据域
+	q->next=p->next;
+	p->next=q; 
+	return 1;
+}
+
+void coutlist(Node *L,int k)//k为倒数第k个结点 
+{
+	int cnt=0;
+	int i=0;
+	Node *p=L->next;
+	Node *q=L->next;
+	while(p!=NULL)
+	{
+		p=p->next;
+		cnt++;	
+	}
+	while(i<cnt-k)
+	{
+		q=q->next;
+		i++;
+	}
+	printf("倒数第k个节点是%d",q->data);
+}
+
+int main()
+{
+	Node *list=initlist();
+	insertNode(list,1,1);
+	insertNode(list,2,2);
+	insertNode(list,3,3);
+	insertNode(list,4,4);
+	insertNode(list,5,5);
+	coutlist(list,3);
+	return 0;
+} 
