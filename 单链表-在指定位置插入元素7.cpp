a0b037f6e840d9f0cec890bbe11f52d36cb4ea81
Zeta0080-cell <2489889104@qq.com>
2025-04-29 18:21:30 +0800

    Add files via upload

---
+++ 单链表-在指定位置插入元素7.cpp
@@ -0,0 +1,55 @@
+//单链表-在指定位置插入元素7 
+#include<bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+typedef struct node{
+	ElemType data;
+	struct node *next;
+}Node;
+
+Node* initlist()
+{
+	Node *head=(Node*)malloc(sizeof(Node));
+	head->data=0;
+	head->next=NULL;
+	return head;
+}
+
+void nodelist(Node *L)
+{
+	Node *p=L->next;
+	while(p!=NULL)
+	{
+		printf("%d ",p->data);
+		p=p->next;
+	}	
+} 
+
+int insertNode(Node *L,int pos,ElemType e)
+{
+	Node* p=L;
+	int i=0;
+	while(i<pos-1)
+	{
+		p=p->next;
+		i++;
+		if(p==NULL)
+		{
+			return 0;
+		}	
+	}	
+	Node *q=(Node*)malloc(sizeof(Node));
+	q->data=e;
+	q->next=p->next;
+	p->next=q;
+} 
+
+int main()
+{
+	Node *list=initList();
+	insertNode(list,1,10);
+	insertNode(list,2,20);
+	insertNode(list,3,30);
+	listNode(list);
+	return 0;
+}
