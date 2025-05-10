1e453282a8acdb10169d597ab247567e362f9923
Zeta0080-cell <2489889104@qq.com>
2025-05-10 18:15:41 +0800

    Add files via upload

---
+++ 单链表-删除中间节点2.cpp
@@ -0,0 +1,76 @@
+//单链表-删除中间节点2
+#include<bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+typedef struct node{
+	ElemType data;
+	struct node *next;
+}Node; 
+
+Node* initlist()
+{
+	Node *head=(Node*)malloc(sizeof(Node));
+	head->data=0;
+	head->next=NULL;
+	return head;
+}
+
+void nodelist(Node *L)
+{
+	Node *p=L->next;
+	while(p!=NULL)
+	{
+		printf("%d",p->data);
+		p=p->next;
+	}
+	printf("\n");
+}
+
+int insertNode(Node *L,int pos,ElemType e)
+{
+	Node *p=L;
+	int i=0;
+	while(i<pos-1)
+	{
+		p=p->next;
+		i++;
+		if(p==NULL)
+		{
+			return 0;
+		}
+	}
+	Node *q=(Node*)malloc(sizeof(Node));
+	q->data=e;
+	q->next=p->next;
+	p->next=q;
+}
+
+int delMiddleNode(Node *head)
+{
+	Node *fast=head->next;
+	Node *slow=head;
+	while(fast!=NULL&&fast->next!=NULL)//第二个确保fast可以走两步 
+	{
+		fast=fast->next->next;
+		slow=slow->next;
+	}
+	Node *q=slow->next;
+	slow->next=q->next;
+	free(q);
+	return 1;
+}
+
+
+int main()
+{
+	Node *list=initlist();
+	insertNode(list,1,1);
+	insertNode(list,2,2);
+	insertNode(list,3,3);
+	insertNode(list,4,4);
+	insertNode(list,5,5);
+	nodelist(list);
+	delMiddleNode(list);
+	nodelist(list);
+	return 0;
+}
